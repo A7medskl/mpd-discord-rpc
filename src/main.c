0b5d648bb6d66538116244d850ebc7d94701792f
Jalen Adams <leftysolara@gmail.com>
2019-03-05 18:26:25 -0500

    Add command line arguments

--- src/main.c
+++ src/main.c
@@ -5,19 +5,61 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 #define MPD_DEFAULT_HOST "localhost"
 #define MPD_DEFAULT_PORT 6600
 #define MPD_DEFAULT_TIMEOUT 30000
 
+
 volatile sig_atomic_t alarm_fired = 0;
+
+int get_args(int argc, char **argv, char **mpd_host, int *mpd_port, int *mpd_timeout)
+{
+    int ch;
+    opterr = 0;
+
+    while ((ch = getopt(argc, argv, "h:p:t:")) != -1) {
+        switch (ch) {
+        case 'h':
+            *mpd_host = optarg;
+            break;
+        case 'p':
+            *mpd_port = atoi(optarg);
+            if (*mpd_port == 0) {  /* Argument was non-numeric */
+                fprintf(stderr, "Invalid port number: %s\n", optarg);
+                return 1;
+            }
+            break;
+        case 't':
+            *mpd_timeout = atoi(optarg);
+            if (*mpd_timeout == 0) {  /* Argument was non-numeric */
+                fprintf(stderr, "Invalid timeout: %s\n", optarg);
+                return 1;
+            }
+            break;
+        case '?':
+            if (optopt == 'h' || optopt == 'p' || optopt == 't')
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint(optopt))
+                fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+            else
+                fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+            return 1;
+        default:
+            abort();
+        }
+    }
+    return 0;
+}
 
 void ding(int signum)
 {
     alarm_fired = 1;
 }
 
-int main()
+int main(int argc, char **argv)
 {
     struct sigaction action;
     memset(&action, 0, sizeof(action));
@@ -26,9 +68,15 @@ int main()
     sigaction(SIGQUIT, &action, NULL);
     sigaction(SIGINT, &action, NULL);
 
-    struct mpdclient *mpd = mpdclient_init(MPD_DEFAULT_HOST, MPD_DEFAULT_PORT,
-                                           MPD_DEFAULT_TIMEOUT);
 
+    int mpd_port = MPD_DEFAULT_PORT;
+    int mpd_timeout = MPD_DEFAULT_TIMEOUT;
+    char *mpd_host = MPD_DEFAULT_HOST;
+
+    if (get_args(argc, argv, &mpd_host, &mpd_port, &mpd_timeout))
+        return 1;
+
+    struct mpdclient *mpd = mpdclient_init(mpd_host, mpd_port, mpd_timeout);
     if (!mpd) {
         printf("Unable to connect to MPD. Exiting...\n");
         return 1;
